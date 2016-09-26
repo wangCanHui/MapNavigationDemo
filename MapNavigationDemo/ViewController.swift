491838c6cd55060086e42bd8302c21089c63bc04
wangCanHui <1361432672@qq.com>
2016-09-26 14:40:04 +0800

    Update ViewController.swift

--- MapNavigationDemo/ViewController.swift
+++ MapNavigationDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 注：此方法使用的是百度地图的经纬度
         navTool = CHNavigationTool(currentLocation: CLLocation(latitude: 29.56646671,longitude: 106.47713852), toLocation: CLLocation(latitude: 29.697116999999999,longitude: 106.61015999999999), viewController: self, title: "请选择地图");
         
     }
